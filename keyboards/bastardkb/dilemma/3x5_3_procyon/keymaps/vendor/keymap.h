1dd64e0b1045c84149b13ef355ba5a643ff13282
Quentin <bstkbd@gmail.com>
2026-01-20 13:54:46 +0100

    Update keymap.h

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.h
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.h
@@ -6,6 +6,8 @@ static uint8_t prev_layer;
 
 void bk_display_layer_number(void);
 void bk_render_mods(uint16_t x, uint16_t y,bool render_all); 
+const char *layer_str(enum dilemma_keymap_layers layer);
+const hsv_t  (enum dilemma_keymap_layers layer);
 
 // static const char *layer_strings[] = {
 //     [LAYER_BASE]       = "00 BASE",
