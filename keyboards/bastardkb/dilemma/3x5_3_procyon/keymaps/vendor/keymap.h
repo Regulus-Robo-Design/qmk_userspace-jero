8f50df7f18b34310dfb63bc116f5259b35f2088f
Quentin <bstkbd@gmail.com>
2026-01-22 14:57:33 +0100

    Update keymap.h

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.h
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.h
@@ -3,10 +3,11 @@
 // painter_font_handle_t font_menu_off;
 static uint8_t prev_layer;
 
-#undef RP_PWM_USE_PWM2
-#define RP_PWM_USE_PWM2 TRUE
 #undef HAL_USE_PWM
 #define HAL_USE_PWM TRUE
+
+#undef RP_PWM_USE_PWM2
+#define RP_PWM_USE_PWM2 TRUE
 
 #undef RP_I2C_USE_I2C0
 #define RP_I2C_USE_I2C0 FALSE
