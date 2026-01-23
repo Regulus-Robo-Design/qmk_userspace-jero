b31108d29e3df0ea41db6200607a43713e26189e
Quentin <bstkbd@gmail.com>
2026-01-23 13:42:53 +0100

    Update keymap.c

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.c
@@ -263,19 +263,19 @@ const char *bk_layer_str(enum dilemma_keymap_layers layer) {
 const uint8_t bk_layer_color (enum dilemma_keymap_layers layer) {
     switch(layer){
         case LAYER_FUNCTION:
-          return {HSV_BLUE};
+          return (uint8_t ){HSV_BLUE};
         case LAYER_NAVIGATION:
-          return {HSV_AZURE};
+          return (uint8_t ){HSV_AZURE};
         case LAYER_MEDIA:
-          return {HSV_ORANGE};
+          return (uint8_t ){HSV_ORANGE};
         case LAYER_POINTER:
-          return {HSV_GREEN};
+          return (uint8_t ){HSV_GREEN};
         case LAYER_NUMERAL:
-          return {HSV_TEAL};
+          return (uint8_t ){HSV_TEAL};
         case LAYER_SYMBOLS:
-          return {HSV_PURPLE};
+          return (uint8_t ){HSV_PURPLE};
         case LAYER_BASE:
         default: 
-          return {HSV_WHITE};
+          return (uint8_t ){HSV_WHITE};
     }
 }
