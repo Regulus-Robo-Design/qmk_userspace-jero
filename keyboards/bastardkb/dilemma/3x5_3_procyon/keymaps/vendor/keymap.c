38fb91886f8e48b4dd9067fac84e9ab75a0df753
Quentin <bstkbd@gmail.com>
2026-01-29 14:56:33 +0100

    adjust positions

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.c
@@ -253,7 +253,6 @@ void bk_display_layer_info(int x, int y, int layer, painter_font_handle_t font, 
         case LAYER_BASE:
         default:
             qp_drawtext(lcd, x, current_y, font, "MODS");
-            current_y += font->line_height;
             if (bk_mods_have_changed() || rewrite_all) {
                 current_y = bk_layer_base_mods(qp_textwidth(font, "MODS ") + x, current_y, bk_font_menu, bk_font_menu_off, rewrite_all);
             }
