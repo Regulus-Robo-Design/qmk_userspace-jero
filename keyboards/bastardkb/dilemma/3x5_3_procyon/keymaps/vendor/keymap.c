1e364a7a374ea111569e8304e06cb71d81373380
Quentin <bstkbd@gmail.com>
2026-02-03 13:25:15 +0100

    switch to surfac

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.c
@@ -229,10 +229,12 @@ void housekeeping_task_user(void) {
     if (prev_layer != layer) {
         bk_display_layer_name(BKS_LAYER_X, BKS_LAYER_Y, layer, bk_font_layer);
         int clear_y = bk_display_layer_info(BKS_LAYER_X, BKS_LAYER_Y + bk_font_layer->line_height, layer, bk_font_menu, TRUE);
-        qp_rect(lcd, BKS_LAYER_X, clear_y, LCD_WIDTH, LCD_HEIGHT, HSV_BLACK, true); // clean rest of screen
+        // qp_rect(lcd, BKS_LAYER_X, clear_y, LCD_WIDTH, LCD_HEIGHT, HSV_BLACK, true); // clean rest of screen
     } else {
         bk_display_layer_info(BKS_LAYER_X, BKS_LAYER_Y + bk_font_layer->line_height, layer, bk_font_menu, FALSE);
     }
+
+    qp_surface_draw(lcd_buffer, lcd, LCD_OFFSET_X, LCD_OFFSET_Y, FALSE);
 
     last_mods  = mods;
     prev_layer = layer;
@@ -240,8 +242,8 @@ void housekeeping_task_user(void) {
 
 void bk_display_layer_name(int x, int y, int layer, painter_font_handle_t font) {
     hsv_t color = bk_layer_color(layer);
-    qp_rect(lcd, 0, 0, BKS_LAYER_BAR_W, LCD_HEIGHT, color.h, color.s, color.v, true);
-    qp_drawtext(lcd, x, y, font, bk_layer_str(layer));
+    qp_rect(lcd_buffer, 0, 0, BKS_LAYER_BAR_W, LCD_HEIGHT, color.h, color.s, color.v, true);
+    qp_drawtext(lcd_buffer, x, y, font, bk_layer_str(layer));
 }
 
 int bk_display_layer_info(int x, int y, int layer, painter_font_handle_t font, bool rewrite_all) {
@@ -261,7 +263,7 @@ int bk_display_layer_info(int x, int y, int layer, painter_font_handle_t font, b
             break;
         case LAYER_BASE:
         default:
-            qp_drawtext(lcd, x, current_y, font, "MODS");
+            qp_drawtext(lcd_buffer, x, current_y, font, "MODS");
             if (bk_mods_have_changed() || rewrite_all) {
                 current_y = bk_layer_base_mods(qp_textwidth(font, "MODS ") + x, current_y, bk_font_menu, bk_font_menu_off, rewrite_all);
             }
@@ -299,18 +301,18 @@ int bk_layer_base_mods(uint16_t x, uint16_t y, painter_font_handle_t font_on, pa
     uint8_t mods = get_mods();
 
     if (((mods & MOD_MASK_GUI) != (last_mods & MOD_MASK_GUI)) || render_all) {
-        qp_drawtext(lcd, x, y, (mods & MOD_MASK_GUI) ? font_on : font_off, "GUI");
+        qp_drawtext(lcd_buffer, x, y, (mods & MOD_MASK_GUI) ? font_on : font_off, "GUI");
     }
     if (((mods & MOD_MASK_ALT) != (last_mods & MOD_MASK_ALT)) || render_all) {
-        qp_drawtext(lcd, x + mod_column_size, y, (mods & MOD_MASK_ALT) ? font_on : font_off, "ALT");
+        qp_drawtext(lcd_buffer, x + mod_column_size, y, (mods & MOD_MASK_ALT) ? font_on : font_off, "ALT");
     }
 
     if (((mods & MOD_MASK_CTRL) != (last_mods & MOD_MASK_CTRL)) || render_all) {
-        qp_drawtext(lcd, x, y + font_on->line_height, (mods & MOD_MASK_CTRL) ? font_on : font_off, "CTRL");
+        qp_drawtext(lcd_buffer, x, y + font_on->line_height, (mods & MOD_MASK_CTRL) ? font_on : font_off, "CTRL");
     }
 
     if (((mods & MOD_MASK_SHIFT) != (last_mods & MOD_MASK_SHIFT)) || render_all) {
-        qp_drawtext(lcd, x + mod_column_size, y + font_on->line_height, (mods & MOD_MASK_SHIFT) ? font_on : font_off, "SHFT");
+        qp_drawtext(lcd_buffer, x + mod_column_size, y + font_on->line_height, (mods & MOD_MASK_SHIFT) ? font_on : font_off, "SHFT");
     }
 
     return y + font_on->line_height * 2;
