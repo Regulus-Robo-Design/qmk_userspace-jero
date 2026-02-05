d8f2b189bdfde18b51998aac74ac2a28f594a622
Quentin <bstkbd@gmail.com>
2026-02-05 14:40:37 +0100

    Update keymap.c

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.c
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G, KC_H, LSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_QUOT),
        PT_Z,    RALT_T(KC_X),    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  RALT_T(KC_DOT), PT_SLSH,
                       ESC_MED, TAB_FUN, SPC_NAV, ENT_SYM, BSP_NUM, KC_MUTE
-  )
+  ),
 
 /*
  * Layers used on the Dilemma.
