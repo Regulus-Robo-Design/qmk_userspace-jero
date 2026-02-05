4307fc1e9964a2238275f5409f44c56f1368d2da
Quentin <bstkbd@gmail.com>
2026-02-05 16:25:06 +0100

    Update keymap.c

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.c
@@ -229,8 +229,8 @@ void housekeeping_task_user(void) {
             const uint8_t layer = get_highest_layer(layer_state);
             const uint8_t mods  = get_mods();
             anim_timer          = timer_read32();
-            qp_clear(surface);
             if (prev_layer != layer) {
+                qp_clear(surface);
                 bk_display_layer_name(BKS_LAYER_X, BKS_LAYER_Y, layer, bk_font_layer);
                 bk_display_layer_info(BKS_LAYER_X, BKS_LAYER_Y + bk_font_layer->line_height, layer, bk_font_menu, TRUE);
                 qp_surface_draw(surface, lcd, 0, 0, true);
