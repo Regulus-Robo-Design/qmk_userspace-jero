3e54004129f9357444011f8445d1274be531bd1f
Quentin <bstkbd@gmail.com>
2026-02-10 13:24:03 +0100

    move get rgb color to main qmk repo

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.c
@@ -283,33 +283,6 @@ int bk_display_layer_info(int x, int y, int layer, painter_font_handle_t font, b
 
 int bk_display_info_media(uint16_t x, uint16_t y, painter_font_handle_t font_on, painter_font_handle_t font_off, bool render_all) {
     int current_y = y;
-
-    // Mods
-    qp_drawtext(surface, x, current_y, font_on, "RGB");
-    int mods_x = qp_textwidth(font_on, "RGB ") + x;
-
-    qp_drawtext(surface, mods_x, current_y, (dilemma_get_pointer_sniping_enabled()) ? font_on : font_off, "SNIPE");
-    qp_drawtext(surface, mods_x, current_y + font_on->line_height + 5, (dilemma_get_pointer_dragscroll_enabled()) ? font_on : font_off, "SCROLL");
-    current_y += font_on->line_height * 2 + 10;
-    // End Mods
-
-    // DPI info
-    char dpi[50];
-    sprintf(dpi, "%u", dilemma_get_pointer_default_dpi());
-    qp_drawtext(surface, x, current_y, font_on, "DPI");
-    mods_x = qp_textwidth(font_on, "DPI ") + x;
-    qp_drawtext(surface, mods_x, current_y, font_off, dpi);
-    current_y += font_on->line_height + 5;
-
-    char s_dpi[50];
-    sprintf(s_dpi, "%u", dilemma_get_pointer_sniping_dpi());
-    qp_drawtext(surface, x, current_y, font_on, "SP. DPI");
-    mods_x = qp_textwidth(font_on, "SP. DPI ") + x;
-    qp_drawtext(surface, mods_x, current_y, font_off, s_dpi);
-    current_y += font_on->line_height + 5;
-
-    // End DPI info
-
     return current_y;
 }
 
--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.h
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.h
@@ -18,8 +18,6 @@ int bk_display_info_base(uint16_t x, uint16_t y, painter_font_handle_t font_on, 
 int bk_display_info_pointer(uint16_t x, uint16_t y, painter_font_handle_t font_on, painter_font_handle_t font_off, bool render_all);
 int bk_display_info_media(uint16_t x, uint16_t y, painter_font_handle_t font_on, painter_font_handle_t font_off, bool render_all);
 
-static HSV _get_hsv_for_layer_index(uint8_t layer);
-
 static const char *layer_strings[] = {
     "00 BASE",
     "01 FUNCT",
