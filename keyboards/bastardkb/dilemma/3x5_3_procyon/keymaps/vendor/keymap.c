6291ccfd802072b9fb98ab40f9942d7e7b93cac7
Quentin <bstkbd@gmail.com>
2026-02-10 13:35:11 +0100

    Update keymap.c

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.c
@@ -283,6 +283,7 @@ int bk_display_layer_info(int x, int y, int layer, painter_font_handle_t font, b
 
 int bk_display_info_media(uint16_t x, uint16_t y, painter_font_handle_t font_on, painter_font_handle_t font_off, bool render_all) {
     int current_y = y;
+
     return current_y;
 }
 
