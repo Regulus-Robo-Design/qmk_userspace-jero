a1563a47d2149fdbd9ded5eccdfbc7c0608d5c97
Quentin <bstkbd@gmail.com>
2026-02-16 15:10:00 +0100

    Update rules.mk

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/rules.mk
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/rules.mk
@@ -12,8 +12,8 @@ SRC += gfx/regular20grey.qff.c
 SRC += gfx/regular36.qff.c
 SRC += gfx/semibold36.qff.c
 SRC += gfx/jostmedium20.qff.c
-SRC += gfx/jostmlight20.qff.c
-SRC += gfx/jostmlight20grey.qff.c
+SRC += gfx/jostlight20.qff.c
+SRC += gfx/jostlight20grey.qff.c
 SRC += gfx/jostbold36.qff.c
 SRC += gfx/mods.qgf.c
 
