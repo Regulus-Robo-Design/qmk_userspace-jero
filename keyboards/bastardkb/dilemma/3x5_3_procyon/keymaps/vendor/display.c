21044fba4f991e849280eb3e4a3f86dbe09bac32
Quentin <bstkbd@gmail.com>
2026-02-17 13:08:18 +0100

    Update display.c

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
@@ -28,11 +28,15 @@ void display_init(void) {
     lv_obj_set_height(ui_label_layer_name, LV_SIZE_CONTENT); /// 55
     lv_obj_set_x(ui_label_layer_name, 25);
     lv_obj_set_y(ui_label_layer_name, -10);
-    lv_obj_set_align(ui_label_layer_name, LV_ALIGN_LEFT_CENTER);
     lv_event_send(ui_label_layer_name, EVENT_LAYER_CHANGE, NULL);
 
     // Change the screen's background color
     lv_obj_set_style_bg_color(ui_screen, lv_color_hex(000000), LV_PART_MAIN);
+
+    // set theme
+    lv_disp_t * dispp = lv_disp_get_default();
+    lv_theme_t * theme = lv_theme_default_init(dispp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), true, LV_FONT_DEFAULT);
+    lv_disp_set_theme(dispp, theme);
 
     lv_disp_load_scr(ui_screen);
     // end screen stuff
