13ed05aa07058b0f7dc50bd1e9ee141ae386c226
Quentin <bstkbd@gmail.com>
2026-02-17 13:27:41 +0100

    Update display.c

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
@@ -12,7 +12,6 @@ enum ui_user_events {
 const char *ui_layer_strings[] = {"BASE", "FUNCTION", "NAV", "MED/RGB", "POINTER", "NUM", "SYM"};
 
 void display_init(void) {
-
     // Screen stuff
     ui_screen = lv_obj_create(NULL);
 
@@ -20,15 +19,21 @@ void display_init(void) {
     // lv_label_set_text(label, "Hello world");
     // lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 
-    // init layer indication
+    /*
+        Base layer screen
+    */
+    // create base layer screen
     ui_label_layer_name = lv_label_create(ui_screen);
+    // display base layer screen upon init
+    lv_disp_load_scr(ui_screen);
+
     lv_label_set_text(ui_label_layer_name, "Base");
     lv_obj_add_event_cb(ui_label_layer_name, ui_layer_change, EVENT_LAYER_CHANGE, NULL);
     lv_obj_set_width(ui_label_layer_name, LV_SIZE_CONTENT);  /// 81
     lv_obj_set_height(ui_label_layer_name, LV_SIZE_CONTENT); /// 55
     lv_obj_set_x(ui_label_layer_name, 25);
     lv_obj_set_y(ui_label_layer_name, -10);
-    lv_obj_set_style_text_color(ui_screen, lv_color_hex(0xffffff), LV_PART_MAIN);
+    // lv_obj_set_style_text_color(ui_screen, lv_color_hex(0xffffff), LV_PART_MAIN);
     // lv_event_send(ui_label_layer_name, EVENT_LAYER_CHANGE, NULL);
 
     // Change the screen's background color
@@ -38,8 +43,6 @@ void display_init(void) {
     // lv_disp_t * dispp = lv_disp_get_default();
     // lv_theme_t * theme = lv_theme_default_init(dispp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), true, LV_FONT_DEFAULT);
     // lv_disp_set_theme(dispp, theme);
-
-    lv_disp_load_scr(ui_screen);
     // end screen stuff
 
     prev_layer = 99;
@@ -55,7 +58,7 @@ void ui_layer_change(lv_event_t *e) {
 
 void housekeeping_task_screen() {
     if (prev_layer != layer_state) {
-        prev_layer   = layer_state;
+        prev_layer = layer_state;
         lv_event_send(ui_label_layer_name, EVENT_LAYER_CHANGE, NULL);
     }
 }
