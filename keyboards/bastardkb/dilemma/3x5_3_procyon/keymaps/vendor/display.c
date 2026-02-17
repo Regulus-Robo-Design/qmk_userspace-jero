b96569232bcf7807c814f46c45b8e14750c99a96
Quentin <bstkbd@gmail.com>
2026-02-17 13:42:09 +0100

    theme

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
@@ -28,25 +28,21 @@ void display_init(void) {
     lv_disp_load_scr(ui_screen);
 
     lv_label_set_text(ui_label_layer_name, "Base");
-    // lv_obj_align(ui_label_layer_name, LV_ALIGN_CENTER, 0, 0);
     lv_obj_add_event_cb(ui_label_layer_name, ui_layer_change, EVENT_LAYER_CHANGE, NULL);
     lv_obj_set_width(ui_label_layer_name, LV_SIZE_CONTENT);  /// 81
     lv_obj_set_height(ui_label_layer_name, LV_SIZE_CONTENT); /// 55
     lv_obj_set_x(ui_label_layer_name, 25);
     lv_obj_set_y(ui_label_layer_name, -10);
     lv_obj_set_align(ui_label_layer_name, LV_ALIGN_LEFT_MID);
-    // lv_obj_add_flag(ui_label_layer_name, LV_OBJ_FLAG_ADV_HITTEST); 
-    // lv_obj_clear_flag(ui_label_layer_name, LV_OBJ_FLAG_SCROLLABLE);
-    // lv_obj_set_style_text_color(ui_screen, lv_color_hex(0xffffff), LV_PART_MAIN);
     lv_event_send(ui_label_layer_name, EVENT_LAYER_CHANGE, NULL);
 
     // Change the screen's background color
     // lv_obj_set_style_bg_color(ui_screen, lv_color_hex(000000), LV_PART_MAIN);
 
     // set theme
-    // lv_disp_t * dispp = lv_disp_get_default();
-    // lv_theme_t * theme = lv_theme_default_init(dispp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), true, LV_FONT_DEFAULT);
-    // lv_disp_set_theme(dispp, theme);
+    lv_disp_t * dispp = lv_disp_get_default();
+    lv_theme_t * theme = lv_theme_default_init(dispp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), true, LV_FONT_DEFAULT);
+    lv_disp_set_theme(dispp, theme);
     // end screen stuff
 
     prev_layer = 99;
