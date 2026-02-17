98515d77e3bbcbb9f3eb42615144b57abf090d50
Quentin <bstkbd@gmail.com>
2026-02-17 13:39:22 +0100

    Update display.c

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
@@ -28,15 +28,15 @@ void display_init(void) {
     lv_disp_load_scr(ui_screen);
 
     lv_label_set_text(ui_label_layer_name, "Base");
-    lv_obj_align(ui_label_layer_name, LV_ALIGN_CENTER, 0, 0);
+    // lv_obj_align(ui_label_layer_name, LV_ALIGN_CENTER, 0, 0);
     lv_obj_add_event_cb(ui_label_layer_name, ui_layer_change, EVENT_LAYER_CHANGE, NULL);
     lv_obj_set_width(ui_label_layer_name, LV_SIZE_CONTENT);  /// 81
     lv_obj_set_height(ui_label_layer_name, LV_SIZE_CONTENT); /// 55
     lv_obj_set_x(ui_label_layer_name, 25);
     lv_obj_set_y(ui_label_layer_name, -10);
-        lv_obj_set_align(ui_label_layer_name, LV_ALIGN_LEFT_MID);
-    lv_obj_add_flag(ui_label_layer_name, LV_OBJ_FLAG_ADV_HITTEST); /// Flags
-    lv_obj_clear_flag(ui_label_layer_name, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_set_align(ui_label_layer_name, LV_ALIGN_LEFT_MID);
+    // lv_obj_add_flag(ui_label_layer_name, LV_OBJ_FLAG_ADV_HITTEST); 
+    // lv_obj_clear_flag(ui_label_layer_name, LV_OBJ_FLAG_SCROLLABLE);
     // lv_obj_set_style_text_color(ui_screen, lv_color_hex(0xffffff), LV_PART_MAIN);
     lv_event_send(ui_label_layer_name, EVENT_LAYER_CHANGE, NULL);
 
