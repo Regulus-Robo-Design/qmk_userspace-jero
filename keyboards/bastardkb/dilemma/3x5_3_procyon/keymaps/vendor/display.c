59dc0afb5adbfb6ef565e1dad6173b2b6b3badb2
Quentin <bstkbd@gmail.com>
2026-02-17 15:46:51 +0100

    Update display.c

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
@@ -42,17 +42,17 @@ void display_init(void) {
 void style_init_mod_indicator(void){
     /*Create a simple button style*/
     lv_style_init(&style_btn);
-    lv_style_set_radius(&style_btn, 10);
+    lv_style_set_radius(&style_btn, 5);
     lv_style_set_bg_opa(&style_btn, LV_OPA_COVER);
-    lv_style_set_bg_color(&style_btn, lv_palette_lighten(LV_PALETTE_GREY, 3));
-    lv_style_set_bg_grad_color(&style_btn, lv_palette_main(LV_PALETTE_GREY));
+    lv_style_set_bg_color(&style_btn, lv_palette_lighten(LV_PALETTE_DEEP_PURPLE, 1));
+    lv_style_set_bg_grad_color(&style_btn, lv_palette_main(LV_PALETTE_DEEP_PURPLE));
     lv_style_set_bg_grad_dir(&style_btn, LV_GRAD_DIR_VER);
 
     lv_style_set_border_color(&style_btn, lv_color_black());
     lv_style_set_border_opa(&style_btn, LV_OPA_20);
-    lv_style_set_border_width(&style_btn, 2);
+    lv_style_set_border_width(&style_btn, 3);
 
-    lv_style_set_text_color(&style_btn, lv_color_black());
+    lv_style_set_text_color(&style_btn, lv_color_white());
 }
 
 void ui_init_layer_name(lv_obj_t *label, const char* layer_name){
