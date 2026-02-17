56910898d0a53630525a3799dabe743894e3c4e4
Quentin <bstkbd@gmail.com>
2026-02-17 11:09:43 +0100

    Update keymap.c

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/keymap.c
@@ -235,15 +235,8 @@ void keyboard_post_init_user(void) {
     // bk_display_layer_number();
     // keyboard_post_init_user();
     // }
-
-    lv_obj_t *btn = lv_button_create(surface);       /*Add a button the current screen*/
-    lv_obj_set_pos(btn, 10, 10);                                /*Set its position*/
-    lv_obj_set_size(btn, 120, 50);                              /*Set its size*/
-    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL); /*Assign a callback to the button*/
-
-    lv_obj_t *label = lv_label_create(btn); /*Add a label to the button*/
-    lv_label_set_text(label, "Button");     /*Set the labels text*/
-    lv_obj_center(label);
+    mbox1 = lv_msgbox_create(surface, "Test", "ACTIVE", NULL, false); // test
+    
 }
 
 static void btn_event_cb(lv_event_t *e) {
