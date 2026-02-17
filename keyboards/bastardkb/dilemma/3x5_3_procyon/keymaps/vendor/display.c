7c3d5bdfa2b67bf89db037c0d68ca1649d0d596b
Quentin <bstkbd@gmail.com>
2026-02-17 12:38:19 +0100

    Update display.c

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
@@ -36,7 +36,8 @@ void display_init(void) {
 void ui_layer_change(lv_event_t *e) {
     lv_event_code_t event_code = lv_event_get_code(e);
     if (event_code == (uint8_t)EVENT_LAYER_CHANGE) {
-        lv_label_set_text(ui_layer_indicator, "Test");
+        int layer = get_highest_layer(layer_state);
+        lv_label_set_text(ui_layer_indicator, layer_strings[layer]);
     }
 }
 
