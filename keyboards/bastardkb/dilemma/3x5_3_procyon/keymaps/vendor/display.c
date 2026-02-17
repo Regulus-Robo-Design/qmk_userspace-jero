f8cec3cf0cb9f0de07a104416f8f3fed01bd4654
Quentin <bstkbd@gmail.com>
2026-02-17 12:53:07 +0100

    trigger layer change effect

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
@@ -2,7 +2,7 @@
 #include "quantum.h"
 
 lv_obj_t *ui_screen;
-lv_obj_t *ui_layer_indicator;
+lv_obj_t *ui_label_layer_name;
 
 enum ui_user_events {
     EVENT_LAYER_CHANGE = 0,
@@ -12,6 +12,8 @@ enum ui_user_events {
 const char *ui_layer_strings[] = {"BASE", "FUNCTION", "NAV", "MED/RGB", "POINTER", "NUM", "SYM"};
 
 void display_init(void) {
+
+    // Screen stuff
     ui_screen = lv_obj_create(NULL);
 
     // // lv_obj_t *label = lv_label_create(ui_screen);
@@ -19,28 +21,36 @@ void display_init(void) {
     // lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 
     // init layer indication
-    ui_layer_indicator = lv_label_create(ui_screen);
-    lv_label_set_text(ui_layer_indicator, "Hello world");
-    lv_obj_align(ui_layer_indicator, LV_ALIGN_CENTER, 0, 0);
-    lv_obj_add_event_cb(ui_layer_indicator, ui_layer_change, EVENT_LAYER_CHANGE, NULL);
-    lv_obj_set_width(ui_layer_indicator, LV_SIZE_CONTENT);  /// 81
-    lv_obj_set_height(ui_layer_indicator, LV_SIZE_CONTENT); /// 55
-    lv_obj_set_x(ui_layer_indicator, 25);
-    lv_obj_set_y(ui_layer_indicator, -10);
-    lv_obj_set_align(ui_layer_indicator, LV_ALIGN_LEFT_MID);
-    lv_obj_add_flag(ui_layer_indicator, LV_OBJ_FLAG_ADV_HITTEST); /// Flags
-    lv_obj_clear_flag(ui_layer_indicator, LV_OBJ_FLAG_SCROLLABLE);
-    lv_event_send(ui_layer_indicator, EVENT_LAYER_CHANGE, NULL);
+    ui_label_layer_name = lv_label_create(ui_screen);
+    lv_label_set_text(ui_label_layer_name, "Hello world");
+    lv_obj_align(ui_label_layer_name, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_add_event_cb(ui_label_layer_name, ui_layer_change, EVENT_LAYER_CHANGE, NULL);
+    lv_obj_set_width(ui_label_layer_name, LV_SIZE_CONTENT);  /// 81
+    lv_obj_set_height(ui_label_layer_name, LV_SIZE_CONTENT); /// 55
+    lv_obj_set_x(ui_label_layer_name, 25);
+    lv_obj_set_y(ui_label_layer_name, -10);
+    lv_obj_set_align(ui_label_layer_name, LV_ALIGN_LEFT_MID);
+    lv_obj_add_flag(ui_label_layer_name, LV_OBJ_FLAG_ADV_HITTEST); /// Flags
+    lv_obj_clear_flag(ui_label_layer_name, LV_OBJ_FLAG_SCROLLABLE);
+    lv_event_send(ui_label_layer_name, EVENT_LAYER_CHANGE, NULL);
 
     lv_disp_load_scr(ui_screen);
+    // end screen stuff
+
+    prev_layer = 99;
 }
 
 void ui_layer_change(lv_event_t *e) {
     lv_event_code_t event_code = lv_event_get_code(e);
     if (event_code == (uint8_t)EVENT_LAYER_CHANGE) {
         int layer = get_highest_layer(layer_state); // todo test that it's smaller than the max size (compare to size of layer_strings)
-        lv_label_set_text(ui_layer_indicator, ui_layer_strings[layer]);
+        lv_label_set_text(ui_label_layer_name, ui_layer_strings[layer]);
     }
 }
 
-void housekeeping_task_screen() {}
+void housekeeping_task_screen() {
+    if (prev_layer != layer_state) {
+        prev_layer   = layer_state;
+        lv_event_send(ui_label_layer_name, EVENT_LAYER_CHANGE, NULL);
+    }
+}
--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.h
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.h
@@ -1,8 +1,10 @@
 #include "lvgl.h"
 
-extern lv_obj_t * ui_screen;
-extern lv_obj_t * ui_layer_indicator;
+extern lv_obj_t *ui_screen;
+extern lv_obj_t *ui_label_layer_name;
 
 void display_init(void);
-void ui_layer_change(lv_event_t * e);
+void ui_layer_change(lv_event_t *e);
 void housekeeping_task_screen(void);
+
+static uint8_t prev_layer;
