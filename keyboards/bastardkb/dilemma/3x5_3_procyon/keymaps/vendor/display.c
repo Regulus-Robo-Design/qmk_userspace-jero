518998d6dbe4fc97350fb7cc579c0cc727689362
Quentin <bstkbd@gmail.com>
2026-02-18 11:48:17 +0100

    Update display.c

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
@@ -78,7 +78,7 @@ void ui_init_button_mod_indicator(lv_obj_t *button, int x, int y){
     // behaviour
     // lv_label_set_text(label, indicator_name);
     // lv_obj_set_align(label, LV_ALIGN_CENTER);
-    lv_obj_add_event_cb(button, event_screen_base_update_mods, EVENT_MOD_CHANGE, NULL);
+    // lv_obj_add_event_cb(button, event_screen_base_update_mods, EVENT_MOD_CHANGE, NULL);
     lv_obj_add_event_cb(button, event_screen_base_update_mods, LV_EVENT_ALL, NULL);
     lv_obj_add_flag(button, LV_OBJ_FLAG_CHECKABLE);
 
@@ -116,8 +116,9 @@ bool process_records_display(uint16_t keycode, keyrecord_t *record){
     switch (keycode) {
         case KC_Q: // test
         // ui_screen_base_update_mods();
-        lv_event_send(ui_button_mod_gui, EVENT_MOD_CHANGE, NULL);
-        lv_event_send(ui_button_mod_gui, LV_EVENT_CLICKED, NULL);
+        // lv_event_send(ui_button_mod_gui, EVENT_MOD_CHANGE, NULL);
+        // lv_event_send(ui_button_mod_gui, LV_EVENT_CLICKED, NULL);
+        lv_event_send(ui_button_mod_gui, LV_EVENT_PRESSING, NULL);
         break;
     }
     return true;
