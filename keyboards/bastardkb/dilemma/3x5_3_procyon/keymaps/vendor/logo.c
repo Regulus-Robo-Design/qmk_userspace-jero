59cedf297e3c1bc248baf43ef3f95651690054af
Regulus-Robo-Design <rodrinom211@gmail.com>
2026-02-27 19:41:18 +0100

    logo

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/logo.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/logo.c
@@ -1,20 +1,17 @@
 #include "logo.h"
-#include "logo_frames.c"  // Contiene la imagen m000_img
+#include "logo_frames.c" // Contiene el array del logo
+#include "lvgl.h"        // Asegúrese de que lvgl esté incluido
 
-void logo_show_frame(void)
-{
-    // Crear el objeto de imagen
-    lv_obj_t * img_obj = lv_img_create(lv_scr_act()); // pantalla activa
-    lv_img_set_src(img_obj, &m000_img);              // asignar la imagen
-    lv_obj_center(img_obj);                          // centrar
+void logo_show_frame(void) {
+    lv_obj_t *img_obj = lv_img_create(lv_scr_act());
+    lv_img_set_src(img_obj, &0000); // logo_img definido en logo_frames.c
+    lv_obj_center(img_obj);
 
-    // Mantener visible durante 2 segundos
-    uint32_t start = lv_tick_get();
-    while(lv_tick_elaps(start) < 2000) {
-        lv_task_handler();  // refrescar pantalla LVGL
-        lv_delay(5);        // pausa corta
-    }
+    lv_task_handler();  // Renderiza la pantalla una vez
 
-    // Borrar el logo de la pantalla
-    lv_obj_del(img_obj);
+    lv_timer_handler(); // Asegura que se refresque LVGL
+
+    lv_tick_inc(2000);  // Opcional: simular 2 segundos de espera
+    // Alternativamente, use un delay del sistema:
+    lv_delay(2000);     // Esto depende de cómo esté configurado su LVGL
 }
--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/logo.h
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/logo.h
@@ -1,9 +1,6 @@
 #ifndef LOGO_H
 #define LOGO_H
 
-#include "lvgl.h"
-
-// Muestra el logo unos segundos y luego lo borra
 void logo_show_frame(void);
 
 #endif // LOGO_H
