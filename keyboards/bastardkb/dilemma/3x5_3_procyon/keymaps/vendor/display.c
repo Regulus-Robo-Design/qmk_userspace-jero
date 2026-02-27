92745dfbfa0bd6f91f0f708ee601a98c705c2e65
Regulus-Robo-Design <rodrinom211@gmail.com>
2026-02-27 20:45:39 +0100

    Logo

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/display.c
@@ -47,7 +47,7 @@ void display_init(void) {
     */
 
 
-    
+    //logo_show_frame();
   
 
 
--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/logo.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/vendor/logo.c
@@ -1,26 +1,19 @@
 #include "logo.h"
-#include "logo_frames.c"  // contiene la imagen m000_map y el descriptor 0000
-#include <stdint.h>
-#include <stdbool.h>
+#include "logo_frames.c"
 
-// Duración del logo en milisegundos
-#define LOGO_DISPLAY_MS 2000
+void logo_show_frame(void)
+{
+    lv_obj_t *img = lv_img_create(lv_scr_act());
+    lv_img_set_src(img, &logo_img);
+    lv_obj_center(img);
 
-void logo_show_frame(void) {
-    // Crear un objeto de imagen temporal
-    lv_obj_t *logo_img_obj = lv_img_create(lv_scr_act()); // usar pantalla actual
-    lv_img_set_src(logo_img_obj, &0000);                 // usar el descriptor de logo_frames.c
-    lv_obj_center(logo_img_obj);                         // centrar en pantalla
+    lv_timer_handler();
 
-    // Forzar refresco de pantalla para ver la imagen
-    lv_task_handler();
-
-    // Espera activa por LOGO_DISPLAY_MS
     uint32_t start = lv_tick_get();
-    while(lv_tick_elaps(start) < LOGO_DISPLAY_MS) {
-        lv_task_handler();  // mantener animaciones y refresco activo
+    while(lv_tick_elaps(start) < 2000)
+    {
+        lv_timer_handler();
     }
 
-    // Borrar la imagen para continuar con la UI normal
-    lv_obj_del(logo_img_obj);
+    lv_obj_del(img);
 }
